8d464587bfa74c8080d25855ddb7f5bd13289577
Juliette Archambeau <juli.archambeau@orange.fr>
2023-03-31 13:12:53 +0100

    add legend_position as param

--- scripts/functions/make_eucli_plot.R
+++ scripts/functions/make_eucli_plot.R
@@ -1,13 +1,13 @@
 # Function to plot the relationship between the Euclidean distance and the genomic offset
 
-make_eucli_plot <- function(X,Y,colors, color_names, plot_title=NULL, xlim=NULL,ylim=NULL,cex=1.2,pch=19,ylab) {
+make_eucli_plot <- function(X,Y,colors, color_names, plot_title=NULL, xlim=NULL,ylim=NULL,cex=1.2,pch=19,ylab,legend_position="topleft") {
   plot(X, Y, 
        xlab ="Euclidean distance",  ylab =ylab, 
        cex = cex , pch = pch, 
        col = colors,
        xlim = xlim, ylim = ylim,
        main = plot_title)
-  legend("topleft", 
+  legend(legend_position, 
          legend=unique(color_names),
          col=unique(colors),
          bty="n", pch=pch, cex=cex, ncol=1,
