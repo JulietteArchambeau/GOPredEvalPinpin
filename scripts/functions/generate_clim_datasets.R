e742978d78f94d9cda7ec6eb243a5cae8e9e5496
Juliette Archambeau <juli.archambeau@orange.fr>
2023-04-28 10:17:03 +0100

    generate clim datasets for diff SSPs

--- scripts/functions/generate_clim_datasets.R
+++ scripts/functions/generate_clim_datasets.R
@@ -1,26 +1,27 @@
 # Function to generate two sets of present and future climatic variables
 # scaled with the past climatic data
 
+# Past climatic data is point estimate adjusted or not for elevation
 
-generate_clim_datasets <- function(clim_var, clim_past = NULL, clim_fut = NULL){
-  
-# Load the past climatic data
+generate_clim_datasets <- function(clim_var, clim_past = NULL, clim_fut = NULL, clim_past_adj = FALSE, ref_period = "ref_1901_1950"){
+
+if(clim_past_adj==TRUE){adj <- "ADJ"} else {adj <- "noADJ"}  
+
+  # Load the past climatic data
 if(is.null(clim_past)){
-clim_past <- read_csv(here("data/DryadRepo/PopulationCoordinatesPastClimateInformation.csv"),
-                        show_col_types = FALSE) %>%
-  dplyr::select(pop,clim_var$variables) # we keep only the climatic variables of interest
+clim_past <- readRDS(here(paste0("data/ClimaticData/MaritimePinePops/ClimatePopulationLocationPointEstimates_ReferencePeriods_",adj,".rds")))[[ref_period]]$ref_means %>%
+  dplyr::select(pop,any_of(clim_var)) # we keep only the climatic variables of interest
 }
   
-  
-# We extract the scaling parameters, i.e. the mean and the variance  
-scale_params <- lapply(clim_var$variables, function(x){
+  # We extract the scaling parameters, i.e. the mean and the variance  
+scale_params <- lapply(clim_var, function(x){
     
     vec_var <- clim_past[,x] %>% pull()
     
     list(mean = mean(vec_var),
          sd = sd(vec_var))
     
-  }) %>% setNames(clim_var$variables)
+  }) %>% setNames(clim_var)
 
 # We scale the past climatic variables
 clim_past <- clim_past %>% 
@@ -28,18 +29,26 @@ clim_past <- clim_past %>%
   
 # we load the future climatic data of the climatic variables of interest
 if(is.null(clim_fut)){
-clim_fut <- read_csv(here("data/DryadRepo/PopulationCoordinatesFutureClimateInformation.csv"),
-                       show_col_types = FALSE) %>% 
-  dplyr::select(pop,clim_var$variables) # we keep only the climatic variables of interest
+list_clim_fut <- readRDS(here("data/ClimaticData/MaritimePinePops/ClimatePopulationLocationValuesExtractedFromRasters_FiveGCMs_2041-2070_SSP370.rds"))
 }
 
 
+list_clim_fut <- lapply(list_clim_fut, function(clim_fut){
+  
+clim_fut <- clim_fut %>% dplyr::select(pop,gcm,any_of(clim_var))
+
 # We scale the future climatic variables with the scaling parameters of the climatic values from the period 1901-1950
-for(i in clim_var$variables){
-    clim_fut[,i] <- (clim_fut[,i] - scale_params[[i]]$mean) / scale_params[[i]]$sd
-  }
+for(i in clim_var){
+  clim_fut[,i] <- (clim_fut[,i] - scale_params[[i]]$mean) / scale_params[[i]]$sd
+}
+
+return(clim_fut)
+
+})
+
+
 
 clim_df <- list(clim_past = clim_past,
-                clim_fut = clim_fut)
+                clim_fut = list_clim_fut)
   
 }
