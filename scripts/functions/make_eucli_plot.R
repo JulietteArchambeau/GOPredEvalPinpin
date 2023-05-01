e5124356cc49acd7cd9e718d4bc4d1a1831d4127
Juliette Archambeau <juli.archambeau@orange.fr>
2023-05-01 16:49:08 +0100

    function to make go maps

--- scripts/functions/make_eucli_plot.R
+++ scripts/functions/make_eucli_plot.R
@@ -1,8 +1,15 @@
 # Function to plot the relationship between the Euclidean distance and the genomic offset
 
-make_eucli_plot <- function(X,Y,colors, color_names, plot_title=NULL, xlim=NULL,ylim=NULL,cex=1.2,pch=19,ylab,legend_position="topleft") {
+make_eucli_plot <- function(X,Y,
+                            colors, color_names, 
+                            plot_title=NULL, 
+                            xlim=NULL,ylim=NULL,
+                            cex=1.2,pch=19,
+                            cex_legend=0.8,
+                            ylab,xlab ="Euclidean distance",
+                            legend_position="topleft") {
   plot(X, Y, 
-       xlab ="Euclidean distance",  ylab =ylab, 
+       xlab = xlab,  ylab =ylab, 
        cex = cex , pch = pch, 
        col = colors,
        xlim = xlim, ylim = ylim,
@@ -10,7 +17,7 @@ make_eucli_plot <- function(X,Y,colors, color_names, plot_title=NULL, xlim=NULL,
   legend(legend_position, 
          legend=unique(color_names),
          col=unique(colors),
-         bty="n", pch=pch, cex=cex, ncol=1,
+         bty="n", pch=pch, cex=cex_legend, ncol=1,
          title="Main gene pools")
   recordPlot()
 }
